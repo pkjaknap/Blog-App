49da752fad66460ad55f4ae58e5c7fdf422c4477
pkjaknap <ujjwalsinghkashyap12@gmail.com>
2024-05-22 19:51:23 +0530

    post routes done

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(bodyParser.json());
 
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
